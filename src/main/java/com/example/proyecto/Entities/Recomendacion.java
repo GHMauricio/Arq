ada880c909327be8a18f5aa89fea03d8d300c8d9
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 19:38:11 -0500

    Add getters and setters to Recomendacion entity

--- src/main/java/com/example/proyecto/Entities/Recomendacion.java
+++ src/main/java/com/example/proyecto/Entities/Recomendacion.java
@@ -19,4 +19,36 @@ public class Recomendacion {
 
     private LocalDate fechaEnvio;
     private String estadoRecomendacion;
+
+    public Long getIdRecomendacion() {
+        return idRecomendacion;
+    }
+
+    public void setIdRecomendacion(Long idRecomendacion) {
+        this.idRecomendacion = idRecomendacion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDate getFechaEnvio() {
+        return fechaEnvio;
+    }
+
+    public void setFechaEnvio(LocalDate fechaEnvio) {
+        this.fechaEnvio = fechaEnvio;
+    }
+
+    public String getEstadoRecomendacion() {
+        return estadoRecomendacion;
+    }
+
+    public void setEstadoRecomendacion(String estadoRecomendacion) {
+        this.estadoRecomendacion = estadoRecomendacion;
+    }
 }
