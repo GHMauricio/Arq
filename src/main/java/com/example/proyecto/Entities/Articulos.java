cadc356c9af864e21e898863c60276a9581dfb47
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 19:33:31 -0500

    Add getters and setters for Articulos fields

--- src/main/java/com/example/proyecto/Entities/Articulos.java
+++ src/main/java/com/example/proyecto/Entities/Articulos.java
@@ -23,4 +23,60 @@ public class Articulos {
     private String categoriaArticulo;
     private LocalDate fechaPublicacion;
     private String autorArticulo;
+
+    public Long getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(Long idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public Recomendacion getRecomendacion() {
+        return recomendacion;
+    }
+
+    public void setRecomendacion(Recomendacion recomendacion) {
+        this.recomendacion = recomendacion;
+    }
+
+    public String getTituloArticulo() {
+        return tituloArticulo;
+    }
+
+    public void setTituloArticulo(String tituloArticulo) {
+        this.tituloArticulo = tituloArticulo;
+    }
+
+    public String getContenidoArticulo() {
+        return contenidoArticulo;
+    }
+
+    public void setContenidoArticulo(String contenidoArticulo) {
+        this.contenidoArticulo = contenidoArticulo;
+    }
+
+    public String getCategoriaArticulo() {
+        return categoriaArticulo;
+    }
+
+    public void setCategoriaArticulo(String categoriaArticulo) {
+        this.categoriaArticulo = categoriaArticulo;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDate fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public String getAutorArticulo() {
+        return autorArticulo;
+    }
+
+    public void setAutorArticulo(String autorArticulo) {
+        this.autorArticulo = autorArticulo;
+    }
 }
