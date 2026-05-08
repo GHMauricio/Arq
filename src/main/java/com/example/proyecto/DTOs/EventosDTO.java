bac5435c7233714f2a7bdf202b909b0a9feffb38
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 20:10:57 -0500

    Rename EventosDTO to EntrevistaDTO and update fields

--- src/main/java/com/example/proyecto/DTOs/EventosDTO.java
+++ src/main/java/com/example/proyecto/DTOs/EventosDTO.java
@@ -2,28 +2,15 @@ package com.example.proyecto.DTOs;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
-public class EventosDTO {
-    private Long idEvento;
-    private Long idUsuario; // Para la relación ManyToOne
-    private String tituloEvento;
-    private String descripcionEvento;
-    private LocalDateTime fechaInicio;
-    private LocalDateTime fechaFin;
-    private String tipoEvento;
+public class EntrevistaDTO {
+    private Long idEntrevista;
+    private Long idRecomendacion;
+    private LocalDate fechaEntrevista;
+    private String temaEntrevista;
+    private String comentarioEntrevista;
 
-    public EventosDTO(Long idEvento, String tituloEvento, LocalDateTime fechaInicio, LocalDateTime fechaFin, String descripcionEvento, Long idUsuario) {
-        this.idEvento = idEvento;
-        this.tituloEvento = tituloEvento;
-        this.fechaInicio = fechaInicio;
-        this.fechaFin = fechaFin;
-        this.descripcionEvento = descripcionEvento;
-        this.idUsuario = idUsuario;
-    }
 
-    public EventosDTO() {
-
-    }
 }
