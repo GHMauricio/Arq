859727ee7434e1956a4b5601a248e78ff4d97681
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 19:37:49 -0500

    Add getters and setters for ProgresoEmocional fields

--- src/main/java/com/example/proyecto/Entities/ProgresoEmocional.java
+++ src/main/java/com/example/proyecto/Entities/ProgresoEmocional.java
@@ -21,4 +21,52 @@ public class ProgresoEmocional {
     @ManyToOne
     @JoinColumn(name = "idUsuario")
     private Usuario usuario;
+
+    public Long getIdProgreso() {
+        return idProgreso;
+    }
+
+    public void setIdProgreso(Long idProgreso) {
+        this.idProgreso = idProgreso;
+    }
+
+    public LocalDate getFechaProgreso() {
+        return fechaProgreso;
+    }
+
+    public void setFechaProgreso(LocalDate fechaProgreso) {
+        this.fechaProgreso = fechaProgreso;
+    }
+
+    public String getEstadoEmocional() {
+        return estadoEmocional;
+    }
+
+    public void setEstadoEmocional(String estadoEmocional) {
+        this.estadoEmocional = estadoEmocional;
+    }
+
+    public String getComentariosProgreso() {
+        return comentariosProgreso;
+    }
+
+    public void setComentariosProgreso(String comentariosProgreso) {
+        this.comentariosProgreso = comentariosProgreso;
+    }
+
+    public Integer getPuntajeEmocional() {
+        return puntajeEmocional;
+    }
+
+    public void setPuntajeEmocional(Integer puntajeEmocional) {
+        this.puntajeEmocional = puntajeEmocional;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
