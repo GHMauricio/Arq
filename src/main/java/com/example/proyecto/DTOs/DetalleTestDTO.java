f67148503cc2aa776bce5951a5f10806148fb6a1
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 20:09:52 -0500

    Remove DetalleTestDTO and its associated fields
    Removed DetalleTestDTO class and its fields, retaining only DetallesEventoDTO class.

--- src/main/java/com/example/proyecto/DTOs/DetalleTestDTO.java
+++ src/main/java/com/example/proyecto/DTOs/DetalleTestDTO.java
@@ -2,18 +2,6 @@ package com.example.proyecto.DTOs;
 
 import lombok.Data;
 
-@Data
-public class DetalleTestDTO {
-    private Long idDetalleTest;
-    private Long idTest;
-    private String pregunta;
-    private String respuesta;
-    private String observacion;
-
-    package com.example.proyecto.DTOs;
-
-import lombok.Data;
-
 import java.time.LocalTime;
 
 @Data
@@ -25,53 +13,5 @@ public class DetallesEventoDTO {
     private LocalTime horaInicio;
     private LocalTime horaFin;
 
-    public Long getIdDetalleEvento() {
-        return idDetalleEvento;
-    }
-
-    public void setIdDetalleEvento(Long idDetalleEvento) {
-        this.idDetalleEvento = idDetalleEvento;
-    }
-
-    public Long getIdEvento() {
-        return idEvento;
-    }
-
-    public void setIdEvento(Long idEvento) {
-        this.idEvento = idEvento;
-    }
-
-    public String getActividad() {
-        return actividad;
-    }
-
-    public void setActividad(String actividad) {
-        this.actividad = actividad;
-    }
-
-    public String getResponsable() {
-        return responsable;
-    }
-
-    public void setResponsable(String responsable) {
-        this.responsable = responsable;
-    }
-
-    public LocalTime getHoraInicio() {
-        return horaInicio;
-    }
-
-    public void setHoraInicio(LocalTime horaInicio) {
-        this.horaInicio = horaInicio;
-    }
-
-    public LocalTime getHoraFin() {
-        return horaFin;
-    }
-
-    public void setHoraFin(LocalTime horaFin) {
-        this.horaFin = horaFin;
-    }
-}
 
 }
