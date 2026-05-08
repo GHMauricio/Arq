da83c7c2e308c61729279f4f58ed40afaba71329
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 19:38:45 -0500

    Add getters and setters for Test entity fields

--- src/main/java/com/example/proyecto/Entities/Test.java
+++ src/main/java/com/example/proyecto/Entities/Test.java
@@ -21,4 +21,52 @@ public class Test {
     private String estadoEmocional;
     private String notasTest;
     private Double puntajeTest;
+
+    public Long getIdTest() {
+        return idTest;
+    }
+
+    public void setIdTest(Long idTest) {
+        this.idTest = idTest;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public LocalDate getFechaTest() {
+        return fechaTest;
+    }
+
+    public void setFechaTest(LocalDate fechaTest) {
+        this.fechaTest = fechaTest;
+    }
+
+    public String getEstadoEmocional() {
+        return estadoEmocional;
+    }
+
+    public void setEstadoEmocional(String estadoEmocional) {
+        this.estadoEmocional = estadoEmocional;
+    }
+
+    public String getNotasTest() {
+        return notasTest;
+    }
+
+    public void setNotasTest(String notasTest) {
+        this.notasTest = notasTest;
+    }
+
+    public Double getPuntajeTest() {
+        return puntajeTest;
+    }
+
+    public void setPuntajeTest(Double puntajeTest) {
+        this.puntajeTest = puntajeTest;
+    }
 }
