a8cbb9d6a4a4689ca3c99ebb6166eaae0cffb3c9
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 20:10:39 -0500

    Remove unused getters and setters in EntrevistaDTO
    Removed getter and setter methods for properties in EntrevistaDTO.

--- src/main/java/com/example/proyecto/DTOs/EntrevistaDTO.java
+++ src/main/java/com/example/proyecto/DTOs/EntrevistaDTO.java
@@ -12,43 +12,5 @@ public class EntrevistaDTO {
     private String temaEntrevista;
     private String comentarioEntrevista;
 
-    public Long getIdEntrevista() {
-        return idEntrevista;
-    }
 
-    public void setIdEntrevista(Long idEntrevista) {
-        this.idEntrevista = idEntrevista;
-    }
-
-    public Long getIdRecomendacion() {
-        return idRecomendacion;
-    }
-
-    public void setIdRecomendacion(Long idRecomendacion) {
-        this.idRecomendacion = idRecomendacion;
-    }
-
-    public LocalDate getFechaEntrevista() {
-        return fechaEntrevista;
-    }
-
-    public void setFechaEntrevista(LocalDate fechaEntrevista) {
-        this.fechaEntrevista = fechaEntrevista;
-    }
-
-    public String getTemaEntrevista() {
-        return temaEntrevista;
-    }
-
-    public void setTemaEntrevista(String temaEntrevista) {
-        this.temaEntrevista = temaEntrevista;
-    }
-
-    public String getComentarioEntrevista() {
-        return comentarioEntrevista;
-    }
-
-    public void setComentarioEntrevista(String comentarioEntrevista) {
-        this.comentarioEntrevista = comentarioEntrevista;
-    }
 }
