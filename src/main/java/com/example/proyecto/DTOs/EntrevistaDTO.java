af4513af5293559b1a25d24382b82ebee7c1606c
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 19:56:35 -0500

    Add getters and setters to EntrevistaDTO

--- src/main/java/com/example/proyecto/DTOs/EntrevistaDTO.java
+++ src/main/java/com/example/proyecto/DTOs/EntrevistaDTO.java
@@ -11,4 +11,44 @@ public class EntrevistaDTO {
     private LocalDate fechaEntrevista;
     private String temaEntrevista;
     private String comentarioEntrevista;
+
+    public Long getIdEntrevista() {
+        return idEntrevista;
+    }
+
+    public void setIdEntrevista(Long idEntrevista) {
+        this.idEntrevista = idEntrevista;
+    }
+
+    public Long getIdRecomendacion() {
+        return idRecomendacion;
+    }
+
+    public void setIdRecomendacion(Long idRecomendacion) {
+        this.idRecomendacion = idRecomendacion;
+    }
+
+    public LocalDate getFechaEntrevista() {
+        return fechaEntrevista;
+    }
+
+    public void setFechaEntrevista(LocalDate fechaEntrevista) {
+        this.fechaEntrevista = fechaEntrevista;
+    }
+
+    public String getTemaEntrevista() {
+        return temaEntrevista;
+    }
+
+    public void setTemaEntrevista(String temaEntrevista) {
+        this.temaEntrevista = temaEntrevista;
+    }
+
+    public String getComentarioEntrevista() {
+        return comentarioEntrevista;
+    }
+
+    public void setComentarioEntrevista(String comentarioEntrevista) {
+        this.comentarioEntrevista = comentarioEntrevista;
+    }
 }
