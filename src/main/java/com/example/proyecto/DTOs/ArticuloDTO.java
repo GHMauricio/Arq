3df4d7bf7e884c6d2b274611fbce92d6ad9264ab
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-07 19:54:46 -0500

    Add getters and setters to ArticuloDTO

--- src/main/java/com/example/proyecto/DTOs/ArticuloDTO.java
+++ src/main/java/com/example/proyecto/DTOs/ArticuloDTO.java
@@ -13,4 +13,60 @@ public class ArticuloDTO {
     private String categoriaArticulo;
     private LocalDate fechaPublicacion;
     private String autorArticulo;
+
+    public Long getIdArticulo() {
+        return idArticulo;
+    }
+
+    public void setIdArticulo(Long idArticulo) {
+        this.idArticulo = idArticulo;
+    }
+
+    public Long getIdRecomendacion() {
+        return idRecomendacion;
+    }
+
+    public void setIdRecomendacion(Long idRecomendacion) {
+        this.idRecomendacion = idRecomendacion;
+    }
+
+    public String getTituloArticulo() {
+        return tituloArticulo;
+    }
+
+    public void setTituloArticulo(String tituloArticulo) {
+        this.tituloArticulo = tituloArticulo;
+    }
+
+    public String getContenidoArticulo() {
+        return contenidoArticulo;
+    }
+
+    public void setContenidoArticulo(String contenidoArticulo) {
+        this.contenidoArticulo = contenidoArticulo;
+    }
+
+    public String getCategoriaArticulo() {
+        return categoriaArticulo;
+    }
+
+    public void setCategoriaArticulo(String categoriaArticulo) {
+        this.categoriaArticulo = categoriaArticulo;
+    }
+
+    public LocalDate getFechaPublicacion() {
+        return fechaPublicacion;
+    }
+
+    public void setFechaPublicacion(LocalDate fechaPublicacion) {
+        this.fechaPublicacion = fechaPublicacion;
+    }
+
+    public String getAutorArticulo() {
+        return autorArticulo;
+    }
+
+    public void setAutorArticulo(String autorArticulo) {
+        this.autorArticulo = autorArticulo;
+    }
 }
