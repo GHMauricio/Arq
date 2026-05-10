49f75eb48ce9c17e9e4f42f99dfac74067ea8a5a
Fran <framosf2003@gmail.com>
2026-05-10 02:57:42 -0700

    Update DetallesTest.java

--- src/main/java/com/example/proyecto/Entities/DetallesTest.java
+++ src/main/java/com/example/proyecto/Entities/DetallesTest.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Table(name = "DetallesTests")
 @Data
 public class DetallesTest {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDetalleTest;
@@ -18,44 +19,4 @@ public class DetallesTest {
     private String pregunta;
     private String respuesta;
     private String observacion;
-
-    public Long getIdDetalleTest() {
-        return idDetalleTest;
-    }
-
-    public void setIdDetalleTest(Long idDetalleTest) {
-        this.idDetalleTest = idDetalleTest;
-    }
-
-    public Test getTest() {
-        return test;
-    }
-
-    public void setTest(Test test) {
-        this.test = test;
-    }
-
-    public String getPregunta() {
-        return pregunta;
-    }
-
-    public void setPregunta(String pregunta) {
-        this.pregunta = pregunta;
-    }
-
-    public String getRespuesta() {
-        return respuesta;
-    }
-
-    public void setRespuesta(String respuesta) {
-        this.respuesta = respuesta;
-    }
-
-    public String getObservacion() {
-        return observacion;
-    }
-
-    public void setObservacion(String observacion) {
-        this.observacion = observacion;
-    }
 }
