81466b5975ab5c4a3608b129fb130d779fecb7c7
Fran <framosf2003@gmail.com>
2026-05-10 03:49:29 -0700

    Update EntrevistaRepository.java

--- src/main/java/com/example/proyecto/Repositories/EntrevistaRepository.java
+++ src/main/java/com/example/proyecto/Repositories/EntrevistaRepository.java
@@ -2,12 +2,17 @@ package com.example.proyecto.Repositories;
 
 import com.example.proyecto.Entities.Entrevista;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
 import java.util.List;
 
 @Repository
-public interface EntrevistaRepository extends JpaRepository<Entrevista,Long> {
-    List<Entrevista> findByRecomendacionIdRecomendacion(Long idUsuario);
+public interface EntrevistaRepository extends JpaRepository<Entrevista, Long> {
+
+    List<Entrevista> findByRecomendacionIdRecomendacion(Long idRecomendacion);
+
+    @Query("SELECT e FROM Entrevista e WHERE e.temaEntrevista = :temaEntrevista")
+    List<Entrevista> listarPorTema(@Param("temaEntrevista") String temaEntrevista);
 }
