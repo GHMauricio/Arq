954c84c473c4868528b5c4fc83f60f434bdd98ae
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-10 05:56:59 -0500

    Refactor IDetallesEventosService interface methods

--- src/main/java/com/example/proyecto/Servicesinterfaces/IDetallesEventosService.java
+++ src/main/java/com/example/proyecto/Servicesinterfaces/IDetallesEventosService.java
@@ -1,13 +1,15 @@
 package com.example.proyecto.Servicesinterfaces;
 
 import com.example.proyecto.DTOs.DetallesEventoDTO;
-import com.example.proyecto.Entities.DetallesEvento;
 
 import java.util.List;
 
 public interface IDetallesEventosService {
-    public void insertar(DetallesEvento detalle);
-    public List<DetallesEventoDTO> listarPorEvento(Long idEvento);
-    public void eliminar(Long id);
+
+    void insertar(DetallesEventoDTO dto);
+    void update(DetallesEventoDTO dto);
     List<DetallesEventoDTO> listar();
+    List<DetallesEventoDTO> listarPorEvento(Long idEvento);
+    List<DetallesEventoDTO> listarPorActividad(String actividad);
+    void eliminar(Long id);
 }
