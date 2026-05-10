2045348a51207dad2b6b8bf0eaf4b361cee72741
Fran <framosf2003@gmail.com>
2026-05-10 04:20:49 -0700

    Update IEventoService.java

--- src/main/java/com/example/proyecto/Servicesinterfaces/IEventoService.java
+++ src/main/java/com/example/proyecto/Servicesinterfaces/IEventoService.java
@@ -1,20 +1,12 @@
 package com.example.proyecto.Servicesinterfaces;
 
 import com.example.proyecto.DTOs.EventosDTO;
-import com.example.proyecto.Entities.Eventos;
-
 import java.util.List;
 
 public interface IEventoService {
-
+    void guardar(EventosDTO dto);
     List<EventosDTO> listarEventosDTO();
-    // Método para registrar o actualizar un evento
-    Eventos guardar(Eventos evento);
-
-    // Método para obtener todos los eventos vinculados a un usuario
     List<EventosDTO> listarPorUsuario(Long idUsuario);
-
-    void eliminar(Long id);
-
     List<EventosDTO> listarTodo();
+    void eliminar(Long id);
 }
