8a396ad545a3ee9f0ff83cce4b2a95739b4466b1
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-10 03:16:16 -0500

    Add method to list recommendations by descending date
    Added a custom query method to retrieve recommendations ordered by date.

--- src/main/java/com/example/proyecto/Repositories/RecomendacionRepository.java
+++ src/main/java/com/example/proyecto/Repositories/RecomendacionRepository.java
@@ -2,11 +2,16 @@ package com.example.proyecto.Repositories;
 
 import com.example.proyecto.Entities.Recomendacion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface RecomendacionRepository extends JpaRepository<Recomendacion,Long> {
+public interface RecomendacionRepository extends JpaRepository<Recomendacion, Long> {
+
     List<Recomendacion> findByUsuarioIdUsuario(Long idUsuario);
+
+    @Query("SELECT r FROM Recomendacion r ORDER BY r.fechaEnvio DESC")
+    List<Recomendacion> listarPorFechaDescendente();
 }
