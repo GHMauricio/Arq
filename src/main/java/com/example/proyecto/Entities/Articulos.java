06b611af7cf7eb36cab816f2b2986bd67b3ccb46
Fran <framosf2003@gmail.com>
2026-05-10 02:57:00 -0700

    Update Articulos.java

--- src/main/java/com/example/proyecto/Entities/Articulos.java
+++ src/main/java/com/example/proyecto/Entities/Articulos.java
@@ -3,17 +3,16 @@ package com.example.proyecto.Entities;
 import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDate;
-import java.util.List;
 
 @Entity
 @Table(name = "Articulos")
 @Data
 public class Articulos {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idArticulo;
 
-    // Nueva relación según el diagrama
     @ManyToOne
     @JoinColumn(name = "idRecomendacion")
     private Recomendacion recomendacion;
@@ -23,60 +22,4 @@ public class Articulos {
     private String categoriaArticulo;
     private LocalDate fechaPublicacion;
     private String autorArticulo;
-
-    public Long getIdArticulo() {
-        return idArticulo;
-    }
-
-    public void setIdArticulo(Long idArticulo) {
-        this.idArticulo = idArticulo;
-    }
-
-    public Recomendacion getRecomendacion() {
-        return recomendacion;
-    }
-
-    public void setRecomendacion(Recomendacion recomendacion) {
-        this.recomendacion = recomendacion;
-    }
-
-    public String getTituloArticulo() {
-        return tituloArticulo;
-    }
-
-    public void setTituloArticulo(String tituloArticulo) {
-        this.tituloArticulo = tituloArticulo;
-    }
-
-    public String getContenidoArticulo() {
-        return contenidoArticulo;
-    }
-
-    public void setContenidoArticulo(String contenidoArticulo) {
-        this.contenidoArticulo = contenidoArticulo;
-    }
-
-    public String getCategoriaArticulo() {
-        return categoriaArticulo;
-    }
-
-    public void setCategoriaArticulo(String categoriaArticulo) {
-        this.categoriaArticulo = categoriaArticulo;
-    }
-
-    public LocalDate getFechaPublicacion() {
-        return fechaPublicacion;
-    }
-
-    public void setFechaPublicacion(LocalDate fechaPublicacion) {
-        this.fechaPublicacion = fechaPublicacion;
-    }
-
-    public String getAutorArticulo() {
-        return autorArticulo;
-    }
-
-    public void setAutorArticulo(String autorArticulo) {
-        this.autorArticulo = autorArticulo;
-    }
 }
