f7d56f34bed8f45689a1bf526388e439992c83dc
Fran <framosf2003@gmail.com>
2026-05-10 02:59:27 -0700

    Update Recomendacion.java

--- src/main/java/com/example/proyecto/Entities/Recomendacion.java
+++ src/main/java/com/example/proyecto/Entities/Recomendacion.java
@@ -2,13 +2,13 @@ package com.example.proyecto.Entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.time.LocalDate;
 
 @Entity
 @Data
 @Table(name = "Recomendaciones")
 public class Recomendacion {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idRecomendacion;
@@ -19,37 +19,4 @@ public class Recomendacion {
 
     private LocalDate fechaEnvio;
     private String estadoRecomendacion;
-
-    public Long getIdRecomendacion() {
-        return idRecomendacion;
-    }
-
-    public void setIdRecomendacion(Long idRecomendacion) {
-        this.idRecomendacion = idRecomendacion;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public LocalDate getFechaEnvio() {
-        return fechaEnvio;
-    }
-
-    public void setFechaEnvio(LocalDate fechaEnvio) {
-        this.fechaEnvio = fechaEnvio;
-    }
-
-    public String getEstadoRecomendacion() {
-        return estadoRecomendacion;
-    }
-
-    public void setEstadoRecomendacion(String estadoRecomendacion) {
-        this.estadoRecomendacion = estadoRecomendacion;
-    }
-
 }
