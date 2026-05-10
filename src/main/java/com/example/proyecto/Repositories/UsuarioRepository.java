2d5cd86f35625f1193f8c37ba3a797525e3f9c66
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-10 03:21:47 -0500

    Add custom query to list Usuarios by birth date

--- src/main/java/com/example/proyecto/Repositories/UsuarioRepository.java
+++ src/main/java/com/example/proyecto/Repositories/UsuarioRepository.java
@@ -2,10 +2,16 @@ package com.example.proyecto.Repositories;
 
 import com.example.proyecto.Entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
     Usuario findByCorreoUsuario(String correo);
-    //Usuario findOneByNombreUsuario(String username);
+
+    @Query("SELECT u FROM Usuario u ORDER BY u.nacimientoAdolescente ASC")
+    List<Usuario> listarPorNacimientoAdolescenteAscendente();
 }
