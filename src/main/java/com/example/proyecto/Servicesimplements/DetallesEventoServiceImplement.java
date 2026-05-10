29199683d125010573cf66a9e42d0732407297df
Fran <framosf2003@gmail.com>
2026-05-10 01:58:56 -0700

    Update DetallesEventoServiceImplement.java

--- src/main/java/com/example/proyecto/Servicesimplements/DetallesEventoServiceImplement.java
+++ src/main/java/com/example/proyecto/Servicesimplements/DetallesEventoServiceImplement.java
@@ -1,10 +1,10 @@
 package com.example.proyecto.Servicesimplements;
 
-import com.example.proyecto.DTOs.DetallesEventoDTO;
-import com.example.proyecto.Entities.DetallesEvento;
+import com.example.proyecto.DTOs.EntrevistaDTO;
 import com.example.proyecto.Entities.Entrevista;
-import com.example.proyecto.Repositories.DetallesEventoRepository;
-import com.example.proyecto.Servicesinterfaces.IDetallesEventosService;
+import com.example.proyecto.Repositories.EntrevistaRepository;
+import com.example.proyecto.Repositories.UsuarioRepository;
+import com.example.proyecto.Servicesinterfaces.IEntrevistaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,55 +12,48 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
-public class DetallesEventoServiceImplement implements IDetallesEventosService {
-
+public class EntrevistaServiceImplement implements IEntrevistaService {
     @Autowired
-    private DetallesEventoRepository deR;
+    private EntrevistaRepository eR;
 
     @Override
-    public void insertar(DetallesEvento detalle) {
-        deR.save(detalle);
+    public void insertar(Entrevista entrevista) {
+        eR.save(entrevista);
     }
 
     @Override
-    public List<DetallesEventoDTO> listarPorEvento(Long idEvento) {
-        return deR.findByEventoIdEvento(idEvento).stream()
-                .map(d -> {
-                    DetallesEventoDTO dto = new DetallesEventoDTO();
-                    dto.setIdDetalleEvento(d.getIdDetalleEvento());
-                    dto.setIdEvento(d.getEvento().getIdEvento());
-                    dto.setActividad(d.getActividad());
-                    dto.setResponsable(d.getResponsable());
-                    dto.setHoraInicio(d.getHoraInicio());
-                    dto.setHoraFin(d.getHoraFin());
-                    return dto;
-                }).collect(Collectors.toList());
-    }
-
-    @Override
-    public void eliminar(Long id) {
-        if(deR.existsById(id)){
-            deR.deleteById(id);
-        }else{
-            throw new RuntimeException("Ingrese un id valido");
-        }
-    }
-
-    @Override
-    public List<DetallesEventoDTO> listar() {
-        return deR.findAll().stream()
+    public List<EntrevistaDTO> listar() {
+        return eR.findAll().stream()
                 .map(this::entityToDto)
                 .collect(Collectors.toList());
     }
 
-    private DetallesEventoDTO entityToDto(DetallesEvento e){
-        DetallesEventoDTO dto = new DetallesEventoDTO();
-        dto.setIdDetalleEvento(e.getIdDetalleEvento());
-        dto.setActividad(e.getActividad());
-        dto.setResponsable(e.getResponsable());
-        dto.setHoraInicio(e.getHoraInicio());
-        dto.setHoraFin(e.getHoraFin());
-        dto.setIdEvento(e.getEvento().getIdEvento());
+    @Override
+    public List<EntrevistaDTO> listarPorRecomendacion(Long idRecomendacion) {
+        return eR.findByRecomendacionIdRecomendacion(idRecomendacion).stream()
+                .map(this::entityToDto)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void eliminar(Long id) {
+        if(eR.existsById(id)){
+            eR.deleteById(id);
+        }else{
+            throw new RuntimeException("No hay entrevista registrada con este id");
+        }
+    }
+
+    private EntrevistaDTO entityToDto(Entrevista e) {
+        EntrevistaDTO dto = new EntrevistaDTO();
+        dto.setIdEntrevista(e.getIdEntrevista());
+        dto.setFechaEntrevista(e.getFechaEntrevista());
+        dto.setTemaEntrevista(e.getTemaEntrevista());
+        dto.setComentarioEntrevista(e.getComentarioEntrevista());
+
+        if (e.getRecomendacion() != null) {
+            dto.setIdRecomendacion(e.getRecomendacion().getIdRecomendacion());
+        }
         return dto;
     }
 }
