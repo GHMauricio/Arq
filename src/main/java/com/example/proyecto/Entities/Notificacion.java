2385ae51190517993ea7b99f3b67dacedb94b3f2
Fran <framosf2003@gmail.com>
2026-05-10 02:58:50 -0700

    Update Notificacion.java

--- src/main/java/com/example/proyecto/Entities/Notificacion.java
+++ src/main/java/com/example/proyecto/Entities/Notificacion.java
@@ -2,14 +2,13 @@ package com.example.proyecto.Entities;
 
 import jakarta.persistence.*;
 import lombok.Data;
-
 import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Table(name = "Notificaciones")
-
 public class Notificacion {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idNotificacion;
@@ -18,48 +17,7 @@ public class Notificacion {
     private LocalDateTime fechaEnvio;
     private boolean leido = false;
 
-
     @ManyToOne
     @JoinColumn(name = "idUsuario")
     private Usuario usuario;
-
-    public Long getIdNotificacion() {
-        return idNotificacion;
-    }
-
-    public void setIdNotificacion(Long idNotificacion) {
-        this.idNotificacion = idNotificacion;
-    }
-
-    public String getMensajeNotificacion() {
-        return mensajeNotificacion;
-    }
-
-    public void setMensajeNotificacion(String mensajeNotificacion) {
-        this.mensajeNotificacion = mensajeNotificacion;
-    }
-
-    public LocalDateTime getFechaEnvio() {
-        return fechaEnvio;
-    }
-
-    public void setFechaEnvio(LocalDateTime fechaEnvio) {
-        this.fechaEnvio = fechaEnvio;
-    }
-
-    public boolean isLeido() {
-        return leido;
-    }
-
-    public void setLeido(boolean leido) {
-        this.leido = leido;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
 }
