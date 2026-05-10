1973a9a61ce7a7941b50db311c7f2f5ef7ab58ef
Mauricio Cortez <u201617471@upc.edu.pe>
2026-05-10 03:05:26 -0500

    Add method to list tests by score in ascending order

--- src/main/java/com/example/proyecto/Repositories/TestRepository.java
+++ src/main/java/com/example/proyecto/Repositories/TestRepository.java
@@ -2,11 +2,16 @@ package com.example.proyecto.Repositories;
 
 import com.example.proyecto.Entities.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TestRepository  extends JpaRepository<Test, Long> {
+public interface TestRepository extends JpaRepository<Test, Long> {
+
     List<Test> findByUsuarioIdUsuarioOrderByFechaTestDesc(Long idUsuario);
+
+    @Query("SELECT t FROM Test t ORDER BY t.puntajeTest ASC")
+    List<Test> listarPorPuntajeAscendente();
 }
