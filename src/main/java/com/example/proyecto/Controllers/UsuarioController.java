d8d70ee3e0ed37b0cacb8dc96926b645a2f7ce87
Fran <framosf2003@gmail.com>
2026-06-16 12:10:09 -0500

    Update UsuarioController.java

--- src/main/java/com/example/proyecto/Controllers/UsuarioController.java
+++ src/main/java/com/example/proyecto/Controllers/UsuarioController.java
@@ -13,13 +13,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@RequestMapping("Usuarios-general")
+@RequestMapping("/usuarios-general")
 public class UsuarioController {
 
     @Autowired
     private IUsuarioService uS;
 
-    @GetMapping
+    @GetMapping("/listar")
     @PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public List<UsuarioDTO> listar() {
         return uS.listar();
