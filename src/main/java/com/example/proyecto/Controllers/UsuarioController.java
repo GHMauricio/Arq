7ed7b885d9b3195982471b708a4ef405a1886660
Fran <framosf2003@gmail.com>
2026-07-03 19:41:25 -0500

    Update UsuarioController.java

--- src/main/java/com/example/proyecto/Controllers/UsuarioController.java
+++ src/main/java/com/example/proyecto/Controllers/UsuarioController.java
@@ -20,7 +20,7 @@ public class UsuarioController {
     private IUsuarioService uS;
 
     @GetMapping("/listar")
-    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    //@PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public List<UsuarioDTO> listar() {
         return uS.listar();
     }
@@ -36,7 +36,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyAuthority('ADMINISTRADOR', 'PADRE')")
+    //@PreAuthorize("hasAnyAuthority('ADMINISTRADOR', 'PADRE')")
     public ResponseEntity<UsuarioDTO> obtenerUsuario(@PathVariable("id") Long id) {
         try {
             UsuarioDTO dto = uS.listarId(id);
@@ -47,7 +47,7 @@ public class UsuarioController {
     }
 
     @PostMapping("/Registrar")
-    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    //@PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public ResponseEntity<?> registroUsuario(@RequestBody UsuarioRegistroDTO dto) {
         if (dto.getCorreoUsuario() == null || !dto.getCorreoUsuario().contains("@")) {
             return ResponseEntity.badRequest().body("El correo debe contener un '@'");
@@ -69,7 +69,7 @@ public class UsuarioController {
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    //@PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public ResponseEntity<?> modificarUsuario(@PathVariable Long id, @RequestBody UsuarioRegistroDTO dto) {
         if (dto.getCorreoUsuario() == null || !dto.getCorreoUsuario().contains("@")) {
             return ResponseEntity.badRequest().body("El correo debe contener un '@'");
@@ -92,7 +92,7 @@ public class UsuarioController {
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAuthority('ADMINISTRADOR')")
+    //@PreAuthorize("hasAuthority('ADMINISTRADOR')")
     public ResponseEntity<String> eliminarUsuario(@PathVariable Long id) {
         // Solo el ADMINISTRADOR puede eliminar usuarios del sistema
         try {
